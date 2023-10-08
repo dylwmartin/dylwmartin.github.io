8970b38207030dbcabb9e524979decfb8a3643ae
dylan <dylwmartin@gmail.com>
2023-10-08 19:54:44 +0000

    worked on post.sh

--- post.sh
+++ post.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 input_file.txt output_file.html"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 input_file.txt"
     exit 1
 fi
 
@@ -14,26 +14,31 @@ title=$(sed -n '1p' "$input_file")
 date=$(sed -n '2p' "$input_file")
 paragraphs=$(sed -e '1,2d' "$input_file")
 
+touch ./posts/html/$title.html
+
+
+#output_file="$title
+#
 # Create HTML content
-html_content="<h2>$title</h2>
-              <p>Date: $date</p>"
-
+#html_content="<h2>$title</h2>
+#              <p>Date: $date</p>"
+#
 # Loop through paragraphs and add to HTML content
-while IFS= read -r paragraph; do
-    html_content+="\n<p>$paragraph</p>"
-done <<< "$paragraphs"
-
+#while IFS= read -r paragraph; do
+#    html_content+="\n<p>$paragraph</p>"
+#done <<< "$paragraphs"
+#
 # Generate the HTML file
-cat <<EOL > "$output_file"
-<!DOCTYPE html>
-<html>
-<head>
-    <title>$title</title>
-</head>
-<body>
-    $html_content
-</body>
-</html>
-EOL
+#cat <<EOL > "$output_file"
+#<!DOCTYPE html>
+#<html>
+#<head>
+#    <title>$title</title>
+#</head>
+#<body>
+#    $html_content
+#</body>
+#</html>
+#EOL
 
-echo "HTML file generated successfully!"
+#echo "HTML file generated successfully!"
